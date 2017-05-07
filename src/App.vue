bba73106b0bd1b31f507112071a2c65b4548c148
Kitsch <xc648593557@gmail.com>
2017-05-07 18:07:52 +0800

    [build] project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <navbar></navbar>
+</template>
+<script>
+import Navbar from './components/Navbar'
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+<style>
+</style>
